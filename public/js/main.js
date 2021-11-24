dec56b7fa14b0224f0d7f1bcb632c0f7c93a406f
Zafar <naveedulz@gmail.com>
2021-11-24 14:24:51 +0100

    en cours

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,44 @@
-let dark = document.querySelector('.noir');
-let blanc = document.querySelector('.blanc');
+let btnConnexion = document.querySelector(".connexion");
+console.log(btnConnexion);
+let divConnexion = document.querySelector("#pageConnexion")
+console.log(divConnexion);
+let btnClose = document.querySelector("#closeWindow")
+console.log(btnClose);
+let inscription = document.querySelector("#inscription")
+let divInscri = document.querySelector("#formulaireNvx")
+let subCo = document.querySelector("#subCo")
 
+btnConnexion.addEventListener("click", () => {
+    divConnexion.style.display = "block";
+})
+
+btnClose.addEventListener("click", () => {
+    divConnexion.style.display ="none";
+})
+
+inscription.addEventListener("click", () =>{
+    divInscri.style.display = "flex";
+})
+
+subCo.addEventListener("click", () =>{
+    divInscri.style.display = "none";
+})
+
+
+
+let btnNoir = document.querySelector('.noir');
+let btnBlanc = document.querySelector('.blanc');
+
+let sombre = () => {
+    document.body.style = "background-color:black"
+    // logo.style.color = "white"
+}
+
+
+btnNoir.addEventListener('click', sombre)
+
+let blanc = () => {
+    document.body.style = "background-color:white"
+    // logo.style.color ="black"
+}
+btnBlanc.addEventListener('click', blanc)
